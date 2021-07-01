57350bc8e27927018174617934ea508055de1453
Edson M Santos <edsonlucianoms@gmail.com>
2021-07-01 14:19:47 -0400

    Update send_telegram
    Melhorado o termo exibido no input de autenticação

--- send_telegram.py
+++ send_telegram.py
@@ -16,7 +16,7 @@ def sendTelegram(phone):
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
-        client.sign_in(phone, input('Insira o código do Telegram: '))
+        client.sign_in(phone, input('Insira o código de autenticação enviado ao seu Telegram: '))
     try:
         receiver = phone
         client.send_message(receiver, message, parse_mode='html')
