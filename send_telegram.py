a739cad868d725a065d5b878bb0fa126bbd2fdb1
Edson M Santos <edsonlucianoms@gmail.com>
2021-07-02 13:23:52 -0400

    Corrigido número do remetente, Adicionado log
    Corrigido número do remetente que estava pegando o primeiro da lista;
    Adicionado log com erro e sucesso.

--- send_telegram.py
+++ send_telegram.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, sync, events
@@ -5,24 +6,28 @@ import re
 import time
 import os
 import json
+import codecs
 
 api_id = ""
 api_hash = ""
 message = ""
 timeout = 5
+senderPhone = ""
 
 def sendTelegram(phone):
     client = TelegramClient('session', api_id, api_hash)
     client.connect()
     if not client.is_user_authorized():
-        client.send_code_request(phone)
-        client.sign_in(phone, input('Insira o código de autenticação enviado ao seu Telegram: '))
+        client.send_code_request(senderPhone)
+        client.sign_in(senderPhone, input('Insira o código de login do Telegram: '))
     try:
         receiver = phone
         client.send_message(receiver, message, parse_mode='html')
+        saveLog('success.txt', phone)
         print('Mensagem enviada para o número: ' + phone)
     except Exception as e:
         print(e)
+        saveLog('error.txt', phone)
     client.disconnect()
 
 def start(filename):
@@ -43,16 +48,24 @@ def loadConfig():
     if (not os.path.exists('./config.json')):
         print('Arquivo de configuração não encontrado')
     else:
-        fileConfig = open('./config.json', 'r')
+        fileConfig = codecs.open('./config.json', 'r', encoding='utf-8')
         config = json.load(fileConfig)
         global timeout
         global api_id
         global api_hash
         global message
+        global senderPhone
         timeout = config["timeout"]
         api_id = config["api_id"]
         api_hash = config["api_hash"]
         message = config["message"]
+        senderPhone = config["myPhone"]
+        fileConfig.close()
+
+def saveLog(filename, content):
+    f = open(filename, "a")
+    f.write(content + "\r")
+    f.close()
 
 loadConfig()
 start('list.txt')
